db41a2df0ac0d176dce97a0cbfd2deb78d2298b2
Medeea Corina <medeea.corina.gabriela@gmail.com>
2025-04-29 15:16:10 +0300

    primul exercitiu

--- day1/d.py
+++ day1/d.py
@@ -1,2 +1,9 @@
-print("hello")
-print(4)
+a = input("Care e numele tau? ")
+b = input("Din ce oraș ești? ")
+c = int(input("Câți ani ai? "))
+
+# Calculăm vârsta peste 10 ani
+c_peste_10_ani = c + 10
+
+# Afișăm mesajul complet
+print("Salut, " + a + "! Locuiești în " + b + " și ai " + str(c) + " ani. Peste 10 ani vei avea " + str(c_peste_10_ani) + " ani!")
